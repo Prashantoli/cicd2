6278b2f0d1a6a2f74094308632c59455ba16d329
Prashant Oli <137768803+Prashantoli@users.noreply.github.com>
2024-07-19 09:14:02 +0545

    Update App.js

--- all_in_docker/client/src/App.js
+++ all_in_docker/client/src/App.js
@@ -75,7 +75,7 @@ function App() {
   return (
     <div className="App">
       <div className="information">
-        <label>What is your name of your father:</label>
+        <label>What is your name of your mother:</label>
         <input
           type="text"
           onChange={(event) => {
